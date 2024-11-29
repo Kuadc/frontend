ea42afba654d983fffc21195b907f5d674f2df15
Carlos Catalan <contactocatalan@gmail.com>
2024-11-29 10:40:34 -0300

    update data.js

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,5 +1,10 @@
+import img1 from "../assets/images/1.jpg"
+import img2 from "../assets/images/2.jpg"
+import img3 from "../assets/images/2.jpg"
+
+
 export const data=[
-    {id: 0, imgUrl: "src/assets/images/1.jpg"},
-    {id: 1, imgUrl: "src/assets/images/2.jpg"},
-    {id: 2, imgUrl: "src/assets/images/3.jpg"},
+    {id: 0, imgUrl: img1},
+    {id: 1, imgUrl: img2},
+    {id: 2, imgUrl: img3},
 ]
